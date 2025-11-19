e85760d69f6894361208bf7d4f9cf356458927b3
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-11-18 22:37:55 -0300

    Initial commit

---
+++ src/test/kotlin/dev/andrey/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.andrey.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
